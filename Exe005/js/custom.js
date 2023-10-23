e35fb1966c273d530922f77ad9fbdaf86e72b748
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2023-10-23 06:14:34 -0700

    Atribution operators
    Operadores de atribuicao

---
+++ Exe005/js/custom.js
@@ -0,0 +1,19 @@
+document.write("<h1>Operadores de Atribuicao</h1>");
+
+var valor1 = 8;
+var valor2 = 3;
+
+/*valor1 += valor2;
+document.write("somando o 8 " + " por " + valor2 + " por atribuicao e igual a " + valor1);*/
+
+/*valor1 -= valor2;
+document.write("subtraindo o 8 " + " por " + valor2 + " por atribuicao e igual a " + valor1);*/
+
+/*valor1 *= valor2;
+document.write("multiplicando o 8 " + " por " + valor2 + " por atribuicao e igual a " + valor1);*/
+
+/*valor1 /= valor2;
+document.write("dividindo o 8 " + " por " + valor2 + " por atribuicao e igual a " + valor1);*/
+
+valor1 %= valor2;
+document.write("resto da divisao do 8 " + " por " + valor2 + " por atribuicao e igual a " + valor1);
