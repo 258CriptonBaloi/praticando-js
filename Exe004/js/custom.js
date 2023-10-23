868669a31220eab919c036486b989e3345be9190
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2023-10-23 05:56:02 -0700

    Maths Operators
    Operadores matematicos, soma, subtracao, multiplicacao, divisao e restoda divisao.

---
+++ Exe004/js/custom.js
@@ -0,0 +1,28 @@
+document.write("<h1>Operadores matematicos</h1>"+ "<br>");
+
+document.write("Soma de 10 e 3"+ "<br><br>");
+var num1 = 10;
+var num2 = 3;
+var somatorio = num1 + num2;
+document.write("A soma de " + num1 + " + " + num2 + " = " + somatorio);
+/**--------------------------------------------------------------------*/
+
+document.write("<br><br>" + "Subtracao de 10 e 3"+ "<br><br>");
+var subtracao = num1 - num2;
+document.write("A subtracao de " + num1 + " - " + num2 + " = " + subtracao);
+/**--------------------------------------------------------------------*/
+
+document.write("<br><br>" + "Multiplicacao de 10 e 3"+ "<br><br>");
+var multiplicacao = num1 * num2;
+document.write("A multiplicacao de " + num1 + " * " + num2 + " = " + multiplicacao);
+/**--------------------------------------------------------------------*/
+
+document.write("<br><br>" + "Divisao de 10 por 3"+ "<br><br>");
+var divisao = num1 / num2;
+document.write("A Divisao de " + num1 + " * " + num2 + " = " + divisao);
+/**--------------------------------------------------------------------*/
+
+document.write("<br><br>" + "Modulo da divisao entre 10 e 3"+ "<br><br>");
+var modulo = num1 % num2;
+document.write("Modulo da divisao de " + num1 + " % " + num2 + " = " + modulo);
+/**--------------------------------------------------------------------*/
