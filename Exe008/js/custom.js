7d2778baa006239361d6bc0f26157adbc1baf774
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2023-10-23 07:31:08 -0700

    Increment & decrement
    Incremento e decremento

---
+++ Exe008/js/custom.js
@@ -0,0 +1,9 @@
+document.write("<h1>Incremento e decremento</h1>");
+
+var quantidadePessoa = 500;
+quantidadePessoa++;
+document.write("Quantidade de pessoas: " + quantidadePessoa + "<br>");
+
+var qtnPessoa = 500;
+quantidadePessoa--;
+document.write("Quantidade de pessoas: " + qtnPessoa);
