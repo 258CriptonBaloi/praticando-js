98d7e213e7a417cf512f5b364a9f68567c025235
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2023-10-25 09:14:34 -0700

    Using switch

---
+++ Exe010/js/custom.js
@@ -0,0 +1,12 @@
+var tipo=2;
+
+switch(tipo){
+    case 1:
+        document.write("Apartamento");
+        break;
+    case 2:
+        document.write("Casa");
+        break;
+    default:
+    document.write("Nenhum dos valores e corespondente!");
+}
